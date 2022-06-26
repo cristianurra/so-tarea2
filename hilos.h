ef953a18b96d9a032c7f101cb1cecdde78f9d60e
Cristhina Urra Aldunate <40528744+cristianurra@users.noreply.github.com>
2022-06-26 00:47:30 -0400

    Update hilos.h

--- hilos.h
+++ hilos.h
@@ -1,16 +1,30 @@
-static void * thread1(void* arg)
+static void * avion1(void* arg)
 {
-	tiempo_aterrizaje=99			#indica en que momento el avion va a necesitar la pista 
+
 	for (int i=0; i<NR_LOOP; i++){
 		
-		if (i>=tiempo_aterrrizaje){  #significa que ya llego la hora de aterrizar, entonces empezará a solicitar el uso de la pista de aterrizaje.
-		sem_wait(&aeropuerto);  #se verifica que la pista esté en desuso
-		aterrizajes=aterrizajes+1;				#se suma una unidad a la cantidad de aviones que han aterrizado
-		break;	#el avion ha aterrizado por lo que se sale del ciclo y el hilo se termina.
+		if (i>=89){  //significa que ya llego la hora de aterrizar, entonces empezará a solicitar el uso de la pista de aterrizaje.
+		sem_wait(&aeropuerto);  //se verifica que la pista esté en desuso
+		aterrizajes=aterrizajes+1;				//se suma una unidad a la cantidad de aviones que han aterrizado
+		sem_post(&aeropuerto);
+		break;	//el avion ha aterrizado por lo que se sale del ciclo y el hilo se termina.
 		}
 	}
 	
 }
 
 
+static void * avion2(void* arg)
+{
 
+	for (int i=0; i<NR_LOOP; i++){
+		
+		if (i>=13){  //significa que ya llego la hora de aterrizar, entonces empezará a solicitar el uso de la pista de aterrizaje.
+		sem_wait(&aeropuerto);  //se verifica que la pista esté en desuso
+		aterrizajes=aterrizajes+1;				//se suma una unidad a la cantidad de aviones que han aterrizado
+		sem_post(&aeropuerto);
+		break;	//el avion ha aterrizado por lo que se sale del ciclo y el hilo se termina.
+		}
+	}
+	
+}
