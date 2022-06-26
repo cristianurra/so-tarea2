1304114dac7b2937b12b49f77ecc6c180cd702eb
Cristhina Urra Aldunate <40528744+cristianurra@users.noreply.github.com>
2022-06-26 00:09:07 -0400

    Update hilos.h

--- hilos.h
+++ hilos.h
@@ -1,20 +1,13 @@
 static void * thread1(void* arg)
 {
+	tiempo_aterrizaje=99
 	for (int i=0; i<NR_LOOP; i++){
+		
+		if (tiempo_aterrrizaje>=tiempo){  #significa que ya llego la hora de aterrizar, entonces empezará a solicitar el uso de la pista de aterrizaje.
 		sem_wait(&aeropuerto);
 		counter+=1;
 		sem_post(&aeropuerto);
 	}
-	
-}
-
-static void * thread2(void* arg)
-{
-	for (int i=0; i<NR_LOOP; i++){
-		sem_wait(&aeropuerto);
-		counter-=1;
-		sem_post(&aeropuerto);
 	}
-	
 	
 }
