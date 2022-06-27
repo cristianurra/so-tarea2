a2ccf76e45714ef4d3b2fe9f4a912ee9625aeeca
Cristhina Urra Aldunate <40528744+cristianurra@users.noreply.github.com>
2022-06-27 16:41:21 -0400

    Update main.c

--- main.c
+++ main.c
@@ -26,37 +26,58 @@ int main(void)
 {	
 	
 	int n;
-	n=56; //cantidad de aviones a simular
+	n=3; //cantidad de aviones a simular
 	srand(time(0));
-	pthread_t thread_1,thread_2,thread_3,thread_4,thread_5,thread_6,thread_7,thread_8,thread_9,thread_10;
+	
+	int i=0;
+	
+	if(i<n){
+		
+	}
+	
+	pthread_t thread_1;
+	pthread_t thread_2;
+	pthread_t thread_3;
+
+	
+	
 	sem_init(&aeropuerto, 0, 1);
-	pthread_create(&thread_1,NULL,*avion1,NULL);
-	printf("Valor %d\n",aterrizajes);
-	pthread_create(&thread_2 ,NULL,*avion2,NULL);
-	printf("Valor %d\n",aterrizajes);
-	pthread_create(&thread_3 ,NULL,*avion3,NULL);
-	printf("Valor %d\n",aterrizajes);
-	pthread_create(&thread_4 ,NULL,*avion4,NULL);
-	printf("Valor %d\n",aterrizajes);
-	pthread_create(&thread_5 ,NULL,*avion5,NULL);
-	pthread_create(&thread_6 ,NULL,*avion6,NULL);
-	pthread_create(&thread_7 ,NULL,*avion7,NULL);
-	pthread_create(&thread_8 ,NULL,*avion8,NULL);
-	pthread_create(&thread_9 ,NULL,*avion9,NULL);
-	pthread_create(&thread_10 ,NULL,*avion10,NULL);
+	
+	int ii=0;
+
+	if(ii<n){
+		pthread_create(&thread_1,NULL,*avion1,NULL);
+		printf("Valor %d\n",aterrizajes);
+		ii=ii+1;
+	}
+
+	if(ii<n){
+		pthread_create(&thread_2,NULL,*avion2,NULL);
+		printf("Valor %d\n",aterrizajes);
+		ii=ii+1;
+	}
+	
+	if(ii<n){
+		pthread_create(&thread_3,NULL,*avion3,NULL);
+		printf("Valor %d\n",aterrizajes);
+		ii=ii+1;
+	}	
+	
+
+
+
+
+
+
+
+
+
 	
 	
 	pthread_join(thread_1,NULL);
 	pthread_join(thread_2,NULL);
 	pthread_join(thread_3,NULL);
-	pthread_join(thread_4,NULL);
-	pthread_join(thread_5,NULL);
-	pthread_join(thread_6,NULL);
-	pthread_join(thread_7,NULL);
-	pthread_join(thread_8,NULL);
-	pthread_join(thread_9,NULL);
-	pthread_join(thread_10,NULL);
-	//pthread_join(thread_11,NULL);
+
 	
 	printf("Valor %d\n",aterrizajes);
 	return 0;
